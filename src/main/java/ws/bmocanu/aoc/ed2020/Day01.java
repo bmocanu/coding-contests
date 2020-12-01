9f4b434c0c9e6503c7b06030fc1a11a5c62331b8
Bogdan Mocanu <bogdan.mocanu@gmail.com>
2020-12-01 09:47:39 +0200

    Day 01

--- src/main/java/ws/bmocanu/aoc/ed2020/Day01.java
+++ src/main/java/ws/bmocanu/aoc/ed2020/Day01.java
@@ -1,18 +1,42 @@
 package ws.bmocanu.aoc.ed2020;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.LineIterator;
 import ws.bmocanu.aoc.utils.FlexStruct;
 import ws.bmocanu.aoc.utils.Utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day01 {
 
     public static void main(String[] args) {
-//        FlexStruct struct = new FlexStruct().loadFromLineList(Utils.fileAsLineList("/ed2020/test.txt"));
-//        struct.mapDataInsidePoints()
-//                .charToType('#', 1)
-//                .charToType('.', 0);
-//        System.out.println(struct.printToString());
+        List<Integer> intList = new ArrayList<>();
 
-        System.out.println(Utils.smallestCommonMultiplier(new int[]{11, 10, 10}));
+        String fileName = "/ed2020/day01.txt";
+        try (InputStream is = Utils.class.getResourceAsStream(fileName)) {
+            List<String> resultList = new ArrayList<>();
+            LineIterator lineIterator = IOUtils.lineIterator(is, StandardCharsets.UTF_8);
+            while (lineIterator.hasNext()) {
+                intList.add(Integer.parseInt(lineIterator.nextLine()));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load payload from file [" + fileName + "]", e);
+        }
+
+        for (int index1 = 0; index1 < intList.size(); index1++) {
+            for (int index2 = 0; index2 < index1; index2++) {
+                for (int index3 = 0; index3 < index2; index3++) {
+                    if (intList.get(index1) + intList.get(index2) + intList.get(index3) == 2020) {
+                        System.out.println(intList.get(index1) * intList.get(index2) * intList.get(index3));
+                    }
+                }
+            }
+        }
+
     }
 
 }
---
+++ src/main/java/ws/bmocanu/aoc/utils/FlexNumber.java
@@ -0,0 +1,13 @@
+package ws.bmocanu.aoc.utils;
+
+public class FlexNumber {
+
+    private byte[] digits = new byte[]{0};
+
+    // ----------------------------------------------------------------------------------------------------
+
+//    public static FlexNumber fromString(String number) {
+//
+//    }
+
+}
