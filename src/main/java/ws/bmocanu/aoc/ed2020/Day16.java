6f98d2e159655bad9e1c3447dbf126629250c0bf
Bogdan Mocanu <bogdan.mocanu@iquestgroup.com>
2020-12-03 16:45:33 +0200

    Preparation time for next days. Also remember this, to be used a backup for template files for future AoC editions

---
+++ src/main/java/ws/bmocanu/aoc/ed2020/Day16.java
@@ -0,0 +1,18 @@
+package ws.bmocanu.aoc.ed2020;
+
+import java.util.List;
+
+import ws.bmocanu.aoc.support.Log;
+import ws.bmocanu.aoc.utils.FileUtils;
+
+public class Day16 {
+
+    public static void main(String[] args) {
+        List<String> stringLines = FileUtils.fileAsStringPerLineToStringList("day16");
+
+        Log.part1(0);
+
+        Log.part2(0);
+    }
+
+}
