33bf5f6ae8370e305d0bff7cea9ecd073fbdfde7
Bogdan Mocanu <bogdan.mocanu@gmail.com>
2020-12-07 07:54:40 +0200

    Day 7

--- src/main/java/ws/bmocanu/aoc/ed2020/Day07.java
+++ src/main/java/ws/bmocanu/aoc/ed2020/Day07.java
@@ -1,18 +1,92 @@
 package ws.bmocanu.aoc.ed2020;
 
-import java.util.List;
+import java.util.*;
 
 import ws.bmocanu.aoc.support.Log;
+import ws.bmocanu.aoc.support.SBinder;
 import ws.bmocanu.aoc.utils.FileUtils;
 
 public class Day07 {
 
+    public static class Content {
+
+        int number;
+        String bagColour;
+
+        @Override
+        public String toString() {
+            return "Content{" +
+                    "number=" + number +
+                    ", bagColour='" + bagColour + '\'' +
+                    '}';
+        }
+    }
+
+    public static Map<String, List<Content>> bags = new HashMap<>();
+
     public static void main(String[] args) {
-        List<String> stringLines = FileUtils.fileAsStringPerLineToStringList("day07");
+        List<String> stringLines = FileUtils.fileAsStringPerLineToStringList("day07_small");
 
-        Log.part1(0);
+        for (String line : stringLines) {
+            int firstIndex = line.indexOf(" bags contain ");
+            String parent = line.substring(0, firstIndex);
+            String rest = line.substring(firstIndex + " bags contain ".length());
+            StringTokenizer tokenizer = new StringTokenizer(rest, ",");
+            List<Content> parentContent = new ArrayList<>();
+            bags.put(parent, parentContent);
+            SBinder binder = new SBinder("(\\d+) (\\w+ \\w+) bags?.", "number", "bagColour");
+            while (tokenizer.hasMoreTokens()) {
+                String subContent = tokenizer.nextToken().trim();
+                if (subContent.contains("no other")) {
+                    parentContent.add(new Content());
+                } else {
+                    parentContent.add(binder.bind(subContent, Content.class));
+                }
+//                Content content = new Content();
+//                if (!subContent.contains("no other")) {
+//                    content.number = Integer.parseInt(subContent.substring(0, subContent.indexOf(' ')));
+//                    content.bagColour = subContent.substring(subContent.indexOf(' ') + 1,
+//                            subContent.indexOf(" bag"));
+//                }
+//                parentContent.add(content);
+            }
+        }
+        System.out.println(bags);
 
-        Log.part2(0);
+        int total = 0;
+        for (String rootColour : bags.keySet()) {
+            total += solvePart1(rootColour) ? 1 : 0;
+        }
+
+        Log.part1(total);
+
+        Log.part2(solvePart2("shiny gold") - 1);
+    }
+
+    public static boolean solvePart1(String currentColour) {
+        if (bags.get(currentColour).get(0).number == 0) {
+            return false;
+        }
+        boolean localResult = false;
+        for (Content content : bags.get(currentColour)) {
+            if (content.bagColour.equals("shiny gold")) {
+                return true;
+            } else {
+                localResult = localResult || solvePart1(content.bagColour);
+            }
+        }
+        return localResult;
+    }
+
+    public static int solvePart2(String currentColour) {
+        if (bags.get(currentColour).get(0).number == 0) {
+            return 1;
+        }
+        int localResult = 0;
+        for (Content content : bags.get(currentColour)) {
+            localResult = localResult + content.number * solvePart2(content.bagColour);
+        }
+        return localResult + 1;
     }
 
 }
