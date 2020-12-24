8726d5694a356c45f3caae50c822efc45d8ae7b0
Bogdan Mocanu <bogdan.mocanu@gmail.com>
2020-12-24 07:58:33 +0200

    Day 24

---
+++ src/main/java/ws/bmocanu/aoc/ed2020/Day24LobbyHexagonalConwaysGame.java
@@ -0,0 +1,114 @@
+package ws.bmocanu.aoc.ed2020;
+
+import java.util.List;
+
+import ws.bmocanu.aoc.flex.Point;
+import ws.bmocanu.aoc.support.Log;
+import ws.bmocanu.aoc.utils.FileUtils;
+import ws.bmocanu.aoc.utils.Utils;
+import ws.bmocanu.aoc.xbase.SolutionBase;
+
+public class Day24LobbyHexagonalConwaysGame extends SolutionBase {
+
+    static int floorSize = 500;
+    static int startX = floorSize / 2;
+    static int startY = floorSize / 2;
+    static int[][] floor;
+    static String[] commands = new String[]{"se", "sw", "ne", "nw", "e", "w"};
+
+    public static void main(String[] args) {
+        List<String> stringLines = FileUtils.fileAsStringPerLineToStringList(filePath("day24"));
+        floor = Utils.createIntMatrix2(floorSize, floorSize);
+        for (String line : stringLines) {
+            Point cursor = Point.from(startX, startY);
+            int charIndex = 0;
+            while (charIndex < line.length()) {
+                String dir = String.valueOf(line.charAt(charIndex));
+                if (dir.equals("s") || dir.equals("n")) {
+                    charIndex++;
+                    dir += String.valueOf(line.charAt(charIndex));
+                }
+                switch (dir) {
+                    case "se":
+                        cursor.x++;
+                        cursor.y++;
+                        break;
+                    case "sw":
+                        cursor.x--;
+                        cursor.y++;
+                        break;
+                    case "nw":
+                        cursor.x--;
+                        cursor.y--;
+                        break;
+                    case "ne":
+                        cursor.x++;
+                        cursor.y--;
+                        break;
+                    case "e":
+                        cursor.x += 2;
+                        break;
+                    case "w":
+                        cursor.x -= 2;
+                        break;
+                }
+                charIndex++;
+            }
+            floor[cursor.x][cursor.y] = 1 - floor[cursor.x][cursor.y];
+        }
+        Log.part1(Utils.iterateIntMatrix2ToSumOfValues(floor));
+
+        for (int round = 0; round < 100; round++) {
+            int[][] curMat = Utils.cloneIntMatrix2(floor);
+            for (int y = 1; y < floorSize - 1; y++) {
+                for (int x = 2; x < floorSize - 2; x++) {
+                    int blackTiles = 0;
+                    for (String command : commands) {
+                        int cx = x;
+                        int cy = y;
+                        switch (command) {
+                            case "se":
+                                cx++;
+                                cy++;
+                                break;
+                            case "sw":
+                                cx--;
+                                cy++;
+                                break;
+                            case "nw":
+                                cx--;
+                                cy--;
+                                break;
+                            case "ne":
+                                cx++;
+                                cy--;
+                                break;
+                            case "e":
+                                cx += 2;
+                                break;
+                            case "w":
+                                cx -= 2;
+                                break;
+                        }
+                        if (floor[cx][cy] == 1) {
+                            blackTiles++;
+                        }
+                    }
+                    if (floor[x][y] == 1 && (blackTiles == 0 || blackTiles > 2)) {
+                        curMat[x][y] = 0;
+                    }
+                    if (floor[x][y] == 0 && (blackTiles == 2)) {
+                        curMat[x][y] = 1;
+                    }
+                }
+            }
+            floor = curMat;
+        }
+
+        Log.part2(Utils.iterateIntMatrix2ToSumOfValues(floor));
+
+        // 360
+        // 3924
+    }
+
+}
---
+++ src/test/java/ws/bmocanu/aoc/ed2020/Day24LobbyHexagonalConwaysGameTest.java
@@ -0,0 +1,25 @@
+package ws.bmocanu.aoc.ed2020;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ws.bmocanu.aoc.support.Log;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class Day24LobbyHexagonalConwaysGameTest {
+
+    @BeforeEach
+    public void setUp() {
+        Log.reset();
+        Day24LobbyHexagonalConwaysGame.setForcedFilePath("/test-ed2020/day24.txt");
+    }
+
+    @Test
+    public void testCode() {
+        Day24LobbyHexagonalConwaysGame.main(null);
+        assertThat(Log.getPart1Long(), is(360L));
+        assertThat(Log.getPart2Long(), is(3924L));
+    }
+
+}
